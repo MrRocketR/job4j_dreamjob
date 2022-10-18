63166c5e772f93b60b11f11edbf2b0968ecd1091
MrRocketR <kuptsovns@gmail.com>
2022-10-18 16:30:46 +0300

    4. Thymeleaf, Циклы

--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -12,11 +12,11 @@ public class Post {
     public Post() {
     }
 
-    public Post(int id, String name, String description, LocalDate date) {
+    public Post(int id, String name, String description, LocalDate created) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.created = date;
+        this.created = created;
 
     }
 
