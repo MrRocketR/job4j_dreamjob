eb84114035b039b47963055928c38c0c81e31693
MrRocketR <kuptsovns@gmail.com>
2022-10-23 17:13:22 +0300

    1. Слоеная архитектура

--- src/main/java/ru/job4j/dreamjob/controller/PostController.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
-import ru.job4j.dreamjob.services.PostService;
+import ru.job4j.dreamjob.service.PostService;
 
 @Controller
 public class PostController {
