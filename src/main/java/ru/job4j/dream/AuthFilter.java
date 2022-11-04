158986862f4e0347c26be1a9a2b05f2e6ee416fd
MrRocketR <kuptsovns@gmail.com>
2022-11-04 14:56:04 +0300

    3. Filter

--- src/main/java/ru/job4j/dream/AuthFilter.java
+++ src/main/java/ru/job4j/dream/AuthFilter.java
@@ -6,9 +6,15 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class AuthFilter implements Filter {
+
+    private final Set<String> filter = new HashSet<>(Arrays.asList("loginPage",
+            "login", "regUser", "registration"));
 
     @Override
     public void doFilter(
@@ -18,8 +24,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login") || uri.endsWith("regUser")
-                || uri.endsWith("registration")) {
+        if (findUri(uri)) {
             chain.doFilter(req, res);
             return;
         }
@@ -28,5 +33,10 @@ public class AuthFilter implements Filter {
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    private boolean findUri(String uri) {
+        return filter.stream().anyMatch(uri::endsWith);
+
     }
 }
